e5ac74819fedc33bd5b3e0bce1e49d93a9a22bbb
Luiz Francisco <luizfk.marinho@gmail.com>
2025-03-19 17:00:18 -0300

    criando rotas pra home

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import LoginForm from './components/Login'
 import RegisterForm from './components/Register'
+import Home from './pages/Home'
 
 const Rotas = () => (
     <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/home" element={<Home />} />
     </Routes>
 )
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import api from "../services/api";
+import userReducer from "./reducers/user"
 
 export const store = configureStore({
     reducer: {
-        [api.reducerPath]: api.reducer
+        user: userReducer,
+        [api.reducerPath]: api.reducer,
+
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(api.middleware)
